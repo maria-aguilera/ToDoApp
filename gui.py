2dae915796ef1de5b1b979ce8428f802c5a4d264
Maria Aguilera Garcia <maria.aguilera@student.ie.edu>
2023-01-27 23:39:43 +0100

    Implement the complete and exit buttons

--- gui.py
+++ gui.py
@@ -13,7 +13,7 @@ exit_button = sg.Button("Exit")
 window = sg.Window("My To-Do App",
                    layout=[[label],
                            [input_box, add_button],
-                           [list_box,edit_button,complete_button]
+                           [list_box,edit_button,complete_button],
                            [exit_button]],
                    font=("Helvetica", 15))
 while True:
@@ -30,21 +30,27 @@ while True:
             window['todos'].update(values=todos)
 
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo']
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo']
 
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica',15))
         case "Complete":
-            todo_to_complete = values["todos"][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window["todos"].update(values=todos)
-            window["todo"].update(value="")
+            try:
+                todo_to_complete = values["todos"][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window["todos"].update(values=todos)
+                window["todo"].update(value="")
+            except IndexError:
+                sg.popup("Please select an item first.", font=('Helvetica',15))
         case "Exit":
             break
         case 'todos':
