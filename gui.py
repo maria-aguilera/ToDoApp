87259d8952f6fa7fcb28c8e9f53b72c90e072fe0
Maria Aguilera Garcia <maria.aguilera@student.ie.edu>
2023-01-27 23:50:13 +0100

    Change theme, add clock, handle exit and complete error

--- gui.py
+++ gui.py
@@ -1,6 +1,10 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme("Topanga")
+
+clock = sg.Text('',key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
@@ -11,16 +15,14 @@ complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window("My To-Do App",
-                   layout=[[label],
+                   layout=[[clock],[label],
                            [input_box, add_button],
                            [list_box,edit_button,complete_button],
                            [exit_button]],
                    font=("Helvetica", 15))
 while True:
-    event, values = window.read()
-    print(1,event)
-    print(2, values)
-    print(3, values['todos'])
+    event, values = window.read(timeout=200)
+    window['clock'].update(value= time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             todos = functions.get_todos()
@@ -28,7 +30,6 @@ while True:
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-
         case "Edit":
             try:
                 todo_to_edit = values['todos'][0]
