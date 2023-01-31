340fa6d3b468ce34af828c1323ce2fddac662ee9
Maria Aguilera Garcia <maria.aguilera@student.ie.edu>
2023-01-31 06:09:06 +0100

    Create static web interface

--- web.py
+++ web.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import functions
 
+def add_todo():
+    todo = st.session_state["new_todo"]
+    print(todo)
+
+
 todos = functions.get_todos()
 
 st.title("My Todo App")
@@ -10,4 +15,5 @@ st.write("This app is to increase your productivity")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo: ", placeholder="Add a new todo...")
+st.text_input(label="Enter a todo: ", placeholder="Add a new todo...",
+              on_change=add_todo,key='new_todo')
